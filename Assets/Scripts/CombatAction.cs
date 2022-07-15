a67d7b39138204fb7b2c8b7b6a675a7d6b304dd1
Olegsander78 <onkotov@gmail.com>
2022-07-15 13:11:00 +0500

    Сделал Combat Actions

--- Assets/Scripts/CombatAction.cs
+++ Assets/Scripts/CombatAction.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatAction : MonoBehaviour
+[CreateAssetMenu(fileName ="Combat Action", menuName =" New Combat Action")]
+public class CombatAction : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Type
     {
-        
+        Attack = 0,
+        Heal = 1
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string DisplayName;
+    public Type ActionType;
+
+    [Header("Damage")]
+    public int Damage;
+    public GameObject ProjectitlePrefab;
+
+    [Header("Heal")]
+    public int HealAmount;
 }
